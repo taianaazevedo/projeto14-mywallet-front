91fa4c5ef32950b7ebbb2190cae30648fa371ce8
taianaazevedo <taianazev@gmail.com>
2023-01-20 18:56:40 -0300

    login ok

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import TelaCadastro from "./pages/TelaCadastro/TelaCadastro"
 import TelaHome from "./pages/TelaHome/TelaHome"
 import NovaEntrada from "./pages/TelaNovaEntrada/TelaNovaEntrada"
 import NovaSaida from "./pages/TelaNovaSaida/TelaNovaSaida"
+import UsuarioContext from "./Context/UsuarioContext";
+import { useState } from "react";
 
 export default function App() {
+    const [usuarioLogado, setUsuarioLogado] = useState(null)
+
     return (
-        <>
+
+        <UsuarioContext.Provider value={{ usuarioLogado, setUsuarioLogado }}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<TelaLogin />} />
-                    <Route path="/cadastro" element={<TelaCadastro/>}/>
+                    <Route path="/cadastro" element={<TelaCadastro />} />
                     <Route path="/home" element={<TelaHome />} />
-                    <Route path="/nova-entrada" element={<NovaEntrada />}/>
-                    <Route path="nova-saida" element={<NovaSaida/>}/>
+                    <Route path="/nova-entrada" element={<NovaEntrada />} />
+                    <Route path="nova-saida" element={<NovaSaida />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </UsuarioContext.Provider>
     )
 }
--- src/pages/TelaLogin/TelaLogin.js
+++ src/pages/TelaLogin/TelaLogin.js
@@ -1,27 +1,45 @@
-
 import { Container } from "../../components/Container"
 import logo from "../../assets/MyWallet.png"
 import { Tela, Form } from "./style"
 import { Link, useNavigate } from "react-router-dom"
+import { useState, useContext } from "react"
+import axios from "axios"
+import UsuarioContext from "../../Context/UsuarioContext"
+
 
 
 export default function Login() {
-    const navigate = useNavigate()
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const navigate = useNavigate();
+    const { setUsuarioLogado } = useContext(UsuarioContext);
+
+
+
+    function login(e) {
+        e.preventDefault();
+        const body = { email, senha };
+        const promise = axios.post(`${process.env.REACT_APP_API_URL}/`, body);
+        promise.then((res) => {
+            setUsuarioLogado(res.data);
+            navigate("/home");
+        });
+        promise.catch((err) => alert(err.response.data.message))
+    }
 
     return (
         <Container>
             <Tela>
-                <img src={logo} alt=""/>
-                <Form>
-                    <input type="email" placeholder="E-mail" required data-test="email"/>
-                    <input type="password" placeholder="Senha" required data-test="password"/>
-                    <button type="submit" data-test="sign-in-submit"><strong>Entrar</strong></button>
-                    <br />
-                    <Link to="/cadastro">
-                        <p>Primeira vez? Cadastre-se!</p>
-                    </Link>
-
+                <img src={logo} alt="" />
+                <Form onSubmit={login}>
+                    <input type="email" placeholder="E-mail" required data-test="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Senha" required data-test="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+                    <button type="submit" data-test="sign-in-submit">Entrar</button>
                 </Form>
+                <br />
+                <Link to="/cadastro">
+                    <p>Primeira vez? Cadastre-se!</p>
+                </Link>
             </Tela>
         </Container>
     )
